a34f0770d18cd70243a518812b4c26a95125f3fb
matias-ferrero <mferreroc@fi.uba.ar>
2023-06-01 11:04:07 -0300

    Implemento el rehash

--- src/hash.c
+++ src/hash.c
@@ -4,10 +4,11 @@
 #include "hash.h"
 
 #define FACTOR_CARGA_MAXIMO 0.7
+#define CAPACIDAD_MINIMA 3
 
 typedef struct nodo {
-	char *clave;
-	void *elemento;
+	const char *clave;
+	void *valor;
 	nodo_t *siguiente;
 } nodo_t;
 
@@ -17,9 +18,52 @@ struct hash {
 	nodo_t **vector;
 };
 
-size_t funcion_hash(char *clave)
+nodo_t *crear_nodo(const char *clave, void *valor)
 {
+	nodo_t *nodo = malloc(sizeof(nodo_t));
+	if (!nodo)
+		return NULL;
 
+	nodo->clave = clave;
+	nodo->valor = valor;
+	nodo->siguiente = NULL;
+
+	return nodo;
+}
+
+void insertar_nodo(hash_t *hash, size_t posicion, nodo_t *nodo)
+{
+	nodo->siguiente = hash->vector[posicion];
+	hash->vector[posicion] = nodo;
+	hash->cantidad++;
+}
+
+nodo_t *buscar_nodo_por_clave(nodo_t *nodo, const char *clave)
+{
+	if (!nodo)
+		return NULL;
+
+	if (!strcmp(nodo->clave, clave))
+		return nodo;
+
+	return buscar_nodo_por_clave(nodo->siguiente, clave);
+}
+
+nodo_t *quitar_nodo(hash_t *hash, nodo_t *nodo, const char *clave, void **valor)
+{
+	if (!nodo)
+		return NULL;
+
+	if (!strcmp(nodo->clave, clave)) {
+		*valor = nodo->valor;
+		nodo_t *aux = nodo->siguiente;
+		free(nodo);
+		hash->cantidad--;
+		return aux;
+	}
+
+	nodo->siguiente = quitar_nodo(hash, nodo->siguiente, clave, valor);
+	return nodo;
 }
 
 hash_t *hash_crear(size_t capacidad)
@@ -33,85 +77,92 @@ hash_t *hash_crear(size_t capacidad)
 		free(hash);
 		return NULL;
 	}
+
+	if (capacidad < CAPACIDAD_MINIMA)
+		capacidad = CAPACIDAD_MINIMA;
+
 	hash->capacidad = capacidad;
 	hash->cantidad = 0;
 
 	return hash;
 }
 
-nodo_t *crear_nodo(char *clave, void *elemento)
+size_t funcion_hash(const char *clave)
 {
-	nodo_t *nodo = malloc(sizeof(nodo_t));
+	size_t hash = 5381;
+	int c;
+
+	while ((c = *clave++))
+		hash = ((hash << 5) + hash) + c;
+
+	return hash;
+}
+
+bool insertar_nodo_en_nuevo_hash(const char *clave, void *valor,
+				 void *nuevo_hash)
+{
+	hash_t *hash = nuevo_hash;
+	nodo_t *nodo = crear_nodo(clave, valor);
 	if (!nodo)
+		return false;
+
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+	insertar_nodo(hash, posicion, nodo);
+	return true;
+}
+
+hash_t *rehash(hash_t *hash)
+{
+	hash_t *nuevo_hash = hash_crear(hash->capacidad * 2);
+	if (!nuevo_hash)
 		return NULL;
 
-	nodo->clave = clave;
-	nodo->elemento = elemento;
-	nodo->siguiente = NULL;
-
-	return nodo;
-}
-
-nodo_t *buscar_nodo_por_clave(nodo_t *nodo, char *clave)
-{
-	if (!nodo)
+	size_t insertados = hash_con_cada_clave(hash,
+						insertar_nodo_en_nuevo_hash,
+						nuevo_hash);
+	if (insertados != hash->cantidad) {
+		hash_destruir(nuevo_hash);
 		return NULL;
+	}
+	hash_t aux = *hash;
+	*hash = *nuevo_hash;
+	*nuevo_hash = aux;
 
-	if (!strcmp(nodo->clave, clave))
-		return nodo;
-
-	return buscar_nodo_por_clave(nodo->siguiente, clave);
+	hash_destruir(nuevo_hash);
+	return hash;
 }
 
-void rehash(hash_t *hash)
-{
-
-}
-
-hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
+hash_t *hash_insertar(hash_t *hash, const char *clave, void *valor,
 		      void **anterior)
 {
 	if (!hash || !clave)
 		return NULL;
 
-	size_t posicion = funcion_hash(clave);
+	if (hash->cantidad / hash->capacidad > FACTOR_CARGA_MAXIMO)
+		hash = rehash(hash);
+
+	if (!hash)
+		return NULL;
+
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
 
 	nodo_t *nodo = hash->vector[posicion];
 	while (nodo != NULL) {
 		if (!strcmp(nodo->clave, clave)) {
-			nodo->elemento = elemento;
+			*anterior = nodo->valor;
+			nodo->valor = valor;
 			return hash;
 		}
 
 		nodo = nodo->siguiente;
 	}
-	nodo_t *nodo = crear_nodo(clave, elemento);
+
+	nodo = crear_nodo(clave, valor);
 	if (!nodo)
 		return NULL;
 
-	nodo->siguiente = hash->vector[posicion];
-	hash->vector[posicion] = nodo;
-	hash->cantidad++;
-
+	insertar_nodo(hash, posicion, nodo);
 	return hash;
-}
-
-nodo_t *hash_quitar_nodo(hash_t *hash, nodo_t *nodo, char *clave,
-			 void **elemento)
-{
-	if (!nodo)
-		return NULL;
-
-	if (!strcmp(nodo->clave, clave)) {
-		*elemento = nodo->elemento;
-		nodo_t *aux = nodo->siguiente;
-		free(nodo);
-		hash->cantidad--;
-		return aux;
-	}
-
-	nodo->siguiente = hash_quitar_nodo(hash, nodo->siguiente, clave, elemento);
-	return nodo;
 }
 
 void *hash_quitar(hash_t *hash, const char *clave)
@@ -119,11 +170,11 @@ void *hash_quitar(hash_t *hash, const char *clave)
 	if (!hash)
 		return NULL;
 
-	void *elemento = NULL;
-	size_t posicion = funcion_hash(clave);
-	hash->vector[posicion] = hash_quitar_nodo(hash, hash->vector[posicion],
-						  clave, &elemento);
-	return elemento;
+	void *valor = NULL;
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
+	hash->vector[posicion] = quitar_nodo(hash, hash->vector[posicion],
+					     clave, &valor);
+	return valor;
 }
 
 void *hash_obtener(hash_t *hash, const char *clave)
@@ -131,9 +182,9 @@ void *hash_obtener(hash_t *hash, const char *clave)
 	if (!hash)
 		return NULL;
 
-	size_t posicion = funcion_hash(clave);
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
 	nodo_t *nodo = buscar_nodo_por_clave(hash->vector[posicion], clave);
-	return nodo->elemento;
+	return nodo->valor;
 }
 
 bool hash_contiene(hash_t *hash, const char *clave)
@@ -141,7 +192,7 @@ bool hash_contiene(hash_t *hash, const char *clave)
 	if (!hash)
 		return NULL;
 
-	size_t posicion = funcion_hash(clave);
+	size_t posicion = funcion_hash(clave) % hash->capacidad;
 	nodo_t *nodo = buscar_nodo_por_clave(hash->vector[posicion], clave);
 	return nodo != NULL;
 }
@@ -169,7 +220,7 @@ void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
 		while (nodo) {			
 			nodo_t *aux = nodo->siguiente;
 			if (destructor != NULL)
-				destructor(nodo->elemento);
+				destructor(nodo->valor);
 
 			free(nodo);
 			hash->cantidad--;
@@ -190,7 +241,7 @@ size_t hash_con_cada_clave(hash_t *hash,
 	for (size_t i = 0; i < hash->capacidad; i++) {
 		nodo_t *nodo = hash->vector[i];
 		while (nodo) {
-			bool validar = f(nodo->clave, nodo->elemento, aux);
+			bool validar = f(nodo->clave, nodo->valor, aux);
 			recorridos++;
 			if (!validar)
 				return recorridos;
